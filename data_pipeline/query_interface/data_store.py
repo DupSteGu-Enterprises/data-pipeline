709fe9e0d8518fbef2576b9e068a1ed61e591568
njdup <njdupoux1994@gmail.com>
2014-08-05 22:37:19 -0700

    Started query_interface politician storing function

--- data_pipeline/query_interface/data_store.py
+++ data_pipeline/query_interface/data_store.py
@@ -5,8 +5,11 @@ Interface for creating models and storing them into the database.
 from models.politician import Politician
 from models.tag import Tag
 from models.funder import Funder
-from models.orm_helper import *
+from models.orm_helper import create_session, create_dbconnection
 
+# TODO: Figure out if db_session should be made global here, or a new one made
+# for each transaction.
+db_session = create_session(create_dbconnection())
 
 def store_politician(politician_data):
     """
@@ -18,4 +21,27 @@ def store_politician(politician_data):
 
     Returns True if the politician has been successfully added to the database.
     """
+    # Replace the list of funder contributions tuples with funder objects
+    politician_data['funders'] = map(extract_funders, politician_data['funders'])
+
+    to_save = Politician(**politician_data) 
+    # TODO: get rid of the exposed derefencing here, not very pythonic...
+    db_session.add(to_save)
+    db_session.commit()
     return True
+
+def extract_funders(funder_information):
+    """
+    Given a tuple of funder information, returns a funder object for that info.
+    NOTE: I think these should instead be dicts of the funder information... 
+    I'm going to continue assuming that its a dict instead
+
+    First checks the database for an already existing entry and returns it if 
+    one exists. Otherwise, creates a new db entry for the funder and stores it
+    in the database.
+    """
+    # TODO: Add check if this funder already exists in database...
+    new_funder = Funder(**funder_information)
+    db_session.add(new_funder)
+    db_session.commit()
+    return new_funder
